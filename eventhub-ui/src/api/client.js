657df2a5b676eaa56c25f70ff8bcb024408e05ec
Shefali Saini <sainishefali120499@gmail.com>
2026-02-27 11:25:00 -0800

    add Axios API client with JWT interceptor and Zustand auth store

---
+++ eventhub-ui/src/api/client.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { getAuthToken, clearAuth } from '../store/authStore';
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+client.interceptors.request.use((config) => {
+  const token = getAuthToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+client.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      clearAuth();
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default client;
