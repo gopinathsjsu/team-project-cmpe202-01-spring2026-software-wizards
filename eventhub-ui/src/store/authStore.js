f9578f0d5e069081abd74fe1ca0759f5f2384b8c
Shefali Saini <sainishefali120499@gmail.com>
2026-02-27 11:25:00 -0800

    feat: add Axios API client with JWT interceptor and Zustand auth store

---
+++ eventhub-ui/src/store/authStore.js
@@ -0,0 +1,19 @@
+import create from 'zustand';
+
+const useAuthStore = create((set) => ({
+  token: null,
+  user: null,
+  setAuth: (token, user) => set({ token, user }),
+  clearAuth: () => set({ token: null, user: null }),
+}));
+
+export const getAuthToken = () => {
+  const state = useAuthStore.getState();
+  return state.token;
+};
+
+export const clearAuth = () => {
+  useAuthStore.getState().clearAuth();
+};
+
+export default useAuthStore;
